d0f7e4babead833258a46696b5f1b0cfd3a1b9aa
Csaba79-coder <csabavadasz79@gmail.com>
2022-09-08 10:24:55 +0200

    pattern code in main

--- src/main/java/csaba79coder/ProducerConsumerPattern.java
+++ src/main/java/csaba79coder/ProducerConsumerPattern.java
@@ -1,8 +1,56 @@
 package csaba79coder;
 
+import csaba79coder.model.Item;
+
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.BlockingDeque;
+
 public class ProducerConsumerPattern {
 
     public static void main(String[] args) {
-        
+
+        // handles concurrent thread access
+        // thread safe datastructures!
+        BlockingDeque<Item> queue = (BlockingDeque<Item>) new ArrayBlockingQueue<Item>(10);
+
+
+        // Producer
+        final Runnable producer = () -> {
+            while (true) {
+                try {
+                    queue.put(new Item().createItem()); // thread blocks if queue is full!
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+        };
+
+        new Thread(producer).start();
+        new Thread(producer).start();
+
+
+        // Consumer
+        final Runnable consumer = () -> {
+            while (true) {
+                try {
+                    Item i = queue.take(); // thread blocks if queue is empty!
+                    process(i);
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+        };
+
+        new Thread(consumer).start();
+        new Thread(consumer).start();
+
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private static void process(Item i) {
     }
 }
---
+++ src/main/java/csaba79coder/model/Item.java
@@ -0,0 +1,8 @@
+package csaba79coder.model;
+
+public class Item {
+
+    public Item createItem() {
+        return new Item();
+    }
+}
